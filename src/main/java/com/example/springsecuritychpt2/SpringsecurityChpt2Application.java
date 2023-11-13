cdadf7873058daa1c4d40f27a4ca947a3b1c7d7e
김준현 <chris1108@dreamsecurity.com>
2023-11-14 08:54:46 +0900

    Initial commit

---
+++ src/main/java/com/example/springsecuritychpt2/SpringsecurityChpt2Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritychpt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityChpt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityChpt2Application.class, args);
+	}
+
+}
